56e072071b01fde735b0e5e5fd57035e42f65eef
rasec1106 <elch.herrera@gmail.com>
2023-09-18 19:39:15 -0500

    Create project

---
+++ Tema4Ejercicio1/src/main/java/pe/edu/cibertec/Tema4Ejercicio1/Tema4Ejercicio1Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Tema4Ejercicio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tema4Ejercicio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tema4Ejercicio1Application.class, args);
+	}
+
+}
---
+++ Tema4Ejercicio1/src/test/java/pe/edu/cibertec/Tema4Ejercicio1/Tema4Ejercicio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Tema4Ejercicio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tema4Ejercicio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
